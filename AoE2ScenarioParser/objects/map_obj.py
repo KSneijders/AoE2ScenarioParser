ae8702d26a70fc4ef5286f892972a9c034d5156d
KSneijders <k.sneijders@st.hanze.nl>
2020-06-20 21:04:47 +0200

    Added type annotations and functionality to map manager

--- AoE2ScenarioParser/objects/map_obj.py
+++ AoE2ScenarioParser/objects/map_obj.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import List
 
 from AoE2ScenarioParser.helper.retriever import find_retriever
@@ -7,31 +9,45 @@ from AoE2ScenarioParser.objects.terrain_obj import TerrainObject
 
 class MapObject(AoE2Object):
     def __init__(self,
-                 map_color_mood,
-                 collide_and_correct,
-                 map_width,
-                 map_height,
-                 terrain
+                 map_color_mood: str,
+                 collide_and_correct: bool,
+                 villager_force_drop: bool,
+                 map_width: int,
+                 map_height: int,
+                 terrain: List[List[TerrainObject]]
                  ):
 
-        self.map_color_mood = map_color_mood
-        self.collide_and_correct = collide_and_correct
-        self._map_width = map_width
-        self._map_height = map_height
-        self.terrain = terrain
+        self.map_color_mood: str = map_color_mood
+        self.collide_and_correct: bool = collide_and_correct
+        self.villager_force_drop: bool = villager_force_drop
+        self._map_width: int = map_width
+        self._map_height: int = map_height
+        self.terrain: List[List[TerrainObject]] = terrain
 
         super().__init__()
 
     @property
-    def map_width(self):
+    def map_size(self) -> int:
+        if self._map_height == self._map_width:
+            return self._map_height
+        else:
+            raise ValueError("Map is not a square. Use the attributes 'map_width' and 'map_height' instead.")
+
+    @map_size.setter
+    def map_size(self, val: int):
+        self._map_width = val
+        self._map_height = val
+
+    @property
+    def map_width(self) -> int:
         return self._map_width
 
     @property
-    def map_height(self):
+    def map_height(self) -> int:
         return self._map_height
 
     @staticmethod
-    def _parse_object(parsed_data, **kwargs):  # Expected {}
+    def _parse_object(parsed_data, **kwargs) -> MapObject:  # Expected {}
         object_piece = parsed_data['MapPiece']
         map_width = find_retriever(object_piece.retrievers, "Map Width").data
         map_height = find_retriever(object_piece.retrievers, "Map Height").data
@@ -56,6 +72,7 @@ class MapObject(AoE2Object):
         return MapObject(
             map_color_mood=find_retriever(object_piece.retrievers, "Map color mood").data,
             collide_and_correct=find_retriever(object_piece.retrievers, "Collide and Correcting").data,
+            villager_force_drop=find_retriever(object_piece.retrievers, "Villager Force Drop").data,
             map_width=map_width,
             map_height=map_height,
             terrain=terrain_2d,
@@ -64,4 +81,3 @@ class MapObject(AoE2Object):
     @staticmethod
     def _reconstruct_object(parsed_header, parsed_data, objects, **kwargs):
         pass
-
