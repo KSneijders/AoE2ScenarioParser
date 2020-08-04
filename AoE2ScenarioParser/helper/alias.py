11661b250af79e89ea5c3cab01e633ac234670f5
pvallet <luap.vallet@gmail.com>
2020-08-04 23:11:52 +0200

    Fixed the Alias class so that it supports any kind of object

--- AoE2ScenarioParser/helper/alias.py
+++ AoE2ScenarioParser/helper/alias.py
@@ -2,17 +2,16 @@ class Alias():
     """
     class that allows to build aliases in handler objects
     so that they can access and modify directly data contained in the pieces
-    Aliases should only be used for immutable objects, and thus do not support complex ones
     """
     def __init__(self, expression):
         self.expression = expression
 
     def __get__(self, instance, owner):
         # Evaluate the expression where self refers to the instance
-        return eval(self.expression, globals(), {'self': instance})
+        return eval(self.expression, {}, {'self': instance})
 
     def __set__(self, instance, value):
         # Execute the expression to assign it the new value
-        exec(self.expression + " = " + str(value), globals(), {'self': instance})
+        exec(self.expression + " = value", {}, {'self': instance, 'value': value})
             
 
