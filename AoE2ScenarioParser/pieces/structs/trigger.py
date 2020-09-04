d2eba4ee6edd1581f6fb8a4be6b291ca2a8add08
KSneijders <k.sneijders@st.hanze.nl>
2020-09-04 19:35:13 +0200

    Renamed all trigger_piece retriever strings to lowercase

--- AoE2ScenarioParser/pieces/structs/trigger.py
+++ AoE2ScenarioParser/pieces/structs/trigger.py
@@ -8,26 +8,26 @@ from AoE2ScenarioParser.pieces.structs.effect import EffectStruct
 class TriggerStruct(AoE2Struct):
     def __init__(self, parser_obj=None, data=None):
         retrievers = [
-            Retriever("Enabled", DataType("u32")),
-            Retriever("Looping", DataType("s8")),
-            Retriever("Description string Table ID", DataType("s32")),
-            Retriever("Act as objective", DataType("u8")),
-            Retriever("Description order (in objectives)", DataType("u32")),
-            Retriever("Make header", DataType("u8")),
-            Retriever("Short description string Table ID", DataType("s32")),
-            Retriever("Display on screen", DataType("u8")),
-            Retriever("Unknown", DataType("5")),
-            Retriever("Mute objectives", DataType("u8")),
+            Retriever("enabled", DataType("u32")),
+            Retriever("looping", DataType("s8")),
+            Retriever("description_string_table_id", DataType("s32")),
+            Retriever("display_as_objective", DataType("u8")),
+            Retriever("objective_description_order", DataType("u32")),
+            Retriever("make_header", DataType("u8")),
+            Retriever("short_description_string_table_id", DataType("s32")),
+            Retriever("display_on_screen", DataType("u8")),
+            Retriever("unknown", DataType("5")),
+            Retriever("mute_objectives", DataType("u8")),
             # Retriever("Trigger starting time", DataType("u32")),
-            Retriever("Trigger description", DataType("str32")),
-            Retriever("Trigger name", DataType("str32")),  # (max 44 characters in UI)
-            Retriever("Short description", DataType("str32")),
-            Retriever("Number of effects", DataType("s32"), save_as="number_of_effects"),
-            Retriever("Effect data", DataType(EffectStruct), set_repeat="{number_of_effects}"),
-            Retriever("Effect display order array", DataType("s32"), set_repeat="{number_of_effects}"),
-            Retriever("Number of conditions", DataType("s32"), save_as="number_of_conditions"),
-            Retriever("Condition data", DataType(ConditionStruct), set_repeat="{number_of_conditions}"),
-            Retriever("Condition display order array", DataType("s32"), set_repeat="{number_of_conditions}"),
+            Retriever("trigger_description", DataType("str32")),
+            Retriever("trigger_name", DataType("str32")),  # (max 44 characters in UI)
+            Retriever("short_description", DataType("str32")),
+            Retriever("number_of_effects", DataType("s32"), save_as="number_of_effects"),
+            Retriever("effect_data", DataType(EffectStruct), set_repeat="{number_of_effects}"),
+            Retriever("effect_display_order_array", DataType("s32"), set_repeat="{number_of_effects}"),
+            Retriever("number_of_conditions", DataType("s32"), save_as="number_of_conditions"),
+            Retriever("condition_data", DataType(ConditionStruct), set_repeat="{number_of_conditions}"),
+            Retriever("condition_display_order_array", DataType("s32"), set_repeat="{number_of_conditions}"),
         ]
 
         super().__init__("Trigger", retrievers, parser_obj, data)
