468e3664028a5b0eb2f0b387dae9e61f3f668234
KSneijders <k.sneijders@st.hanze.nl>
2020-09-07 01:30:45 +0200

    Updated retriever with more functionality for link_list options

--- AoE2ScenarioParser/helper/retriever.py
+++ AoE2ScenarioParser/helper/retriever.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Type, Any, TYPE_CHECKING, List
+from typing import Type, TYPE_CHECKING, List
 
 from AoE2ScenarioParser.helper import helper
 from AoE2ScenarioParser.helper.datatype import DataType
@@ -8,6 +8,7 @@ from AoE2ScenarioParser.helper.datatype import DataType
 if TYPE_CHECKING:
     from AoE2ScenarioParser.pieces.aoe2_piece import AoE2Piece
     from AoE2ScenarioParser.objects.aoe2_object import AoE2Object
+    from typing import OrderedDict
 
 
 class Retriever:
@@ -58,22 +59,23 @@ class RetrieverObjectLink:
                  link: str = None,
                  process_as_object: Type[AoE2Object] = None,
                  retrieve_instance_number: bool = False,
+                 retrieve_history_number=-1
                  ):
-        if link is None and not retrieve_instance_number:
-            raise ValueError("Either the 'link' parameter or 'retrieve_instance_number' parameter need to be defined.")
-        if link is not None and retrieve_instance_number:
-            raise ValueError("Cannot use both the 'link' parameter and 'retrieve_instance_number' parameter.")
+        if (link is not None) + retrieve_instance_number + (retrieve_history_number != -1) != 1:
+            raise ValueError("Use one and only one of the following parameters: 'link', 'retrieve_instance_number' or "
+                             "'retrieve_history_number'.")
 
         if link is not None:
-            link: str = self._process_link(link)
+            link: str = RetrieverObjectLink._process_link(link)
 
         self.name: str = variable_name
         self.link = link
         self.process_as_object: Type[AoE2Object] = process_as_object
         self.retrieve_instance_number: bool = retrieve_instance_number
+        self.retrieve_history_number: int = retrieve_history_number
 
     @staticmethod
-    def _process_link(link) -> str:
+    def _process_link(link: str) -> str:
         dot = link.find(".")
         # Convert piece attribute to dict key: "MapPiece.x" > "pieces['MapPiece'].x"
         link = "pieces['" + link[0:dot] + "']" + link[dot:]
@@ -83,7 +85,7 @@ class RetrieverObjectLink:
         return "[RetrieverObjectLink] " + self.name + ": " + str(self.link) + \
                (" -> " + self.process_as_object.__name__ if self.process_as_object is not None else "")
 
-    def get_piece_datatype(self, pieces: List[AoE2Piece]) -> Type[AoE2Piece]:
+    def get_piece_datatype(self, pieces: OrderedDict[str, AoE2Piece]) -> Type[AoE2Piece]:
         if self.process_as_object is None:
             raise ValueError("Cannot get piece type from RetrieverObjectLink when parameter process_as_object has not "
                              "been set.")
@@ -93,6 +95,21 @@ class RetrieverObjectLink:
             'pieces': dict(pieces),
             'find_retriever': get_retriever_by_name
         })
+
+    # Todo: Remove this ugly piece of code... Migrate _construct() and commit() logic (back) into RetrieverObjectLink?
+    def _process_special_link(self, pieces: OrderedDict[str, AoE2Piece]):
+        if "[]" not in self.link:
+            return None
+        else:
+            list_notation_location = self.link.find("[]")
+            struct_list = eval(self.link[:list_notation_location], {}, {'pieces': pieces})
+            list_len = len(struct_list)
+            result_list = [[] for _ in range(0, list_len)]
+            for i in range(0, list_len):
+                list_of_attributes = struct_list[i].__getattr__(self.link[list_notation_location+3:])
+                for j in range(0, len(list_of_attributes)):
+                    result_list[i].append(self.process_as_object._construct(pieces, [i, j]))
+            return result_list
 
 
 def get_retriever_by_name(retriever_list: List[Retriever], name: str) -> (Retriever, RetrieverObjectLink):
