da7b1379ccebd09463d28033726446eaf1e811ed
KSneijders <k.sneijders@st.hanze.nl>
2020-10-28 00:01:26 +0100

    Added new Dependencies to options piece

--- AoE2ScenarioParser/pieces/options.py
+++ AoE2ScenarioParser/pieces/options.py
@@ -1,28 +1,46 @@
-from AoE2ScenarioParser.pieces import aoe2_piece
-from AoE2ScenarioParser.helper.retriever import Retriever
 from AoE2ScenarioParser.helper.datatype import DataType
+from AoE2ScenarioParser.helper.retriever import Retriever
+from AoE2ScenarioParser.helper.retriever_dependency import RetrieverDependency, DependencyTarget, DependencyAction, \
+    DependencyEval
+from AoE2ScenarioParser.pieces import aoe2_piece
 
 
 class OptionsPiece(aoe2_piece.AoE2Piece):
     def __init__(self, parser_obj=None, data=None):
-        retrievers = [
-            Retriever("per_player_number_of_disabled_techs", DataType("u32", repeat=16), save_as="disabled_techs"),
-            Retriever("disabled_technology_ids_in_player_order", DataType("u32"), set_repeat="sum({disabled_techs})"),
-            # Retriever("per_player_extra_number_of_disabled_techs", DataType("u32", repeat=16)),  # Removed in DE?
-            Retriever("per_player_number_of_disabled_units", DataType("u32", repeat=16), save_as="disabled_units"),
-            Retriever("disabled_unit_ids_in_player_order", DataType("u32"), set_repeat="sum({disabled_units})"),
-            # Retriever("per_player_extra_number_of_disabled_units", DataType("u32", repeat=16)),  # Removed in DE?
-            Retriever("per_player_number_of_disabled_buildings",
-                      DataType("u32", repeat=16), save_as="disabled_buildings"),
-            Retriever("disabled_building_IDs_in_player_order",
-                      DataType("u32"), set_repeat="sum({disabled_buildings})"),
+        retrievers = []
+        for disabled_type in ["tech", "building", "unit"]:
+            retrievers.append(
+                Retriever(f"per_player_number_of_disabled_{disabled_type}s", DataType("u32", repeat=16),
+                          on_refresh=RetrieverDependency(
+                              DependencyAction.SET_VALUE,
+                              DependencyTarget(["self"] * 8,
+                                               [f"disabled_{disabled_type}_ids_player_{p}" for p in range(1, 9)]),
+                              DependencyEval("[len(x) for x in [p1, p2, p3, p4, p5, p6, p7, p8]]",
+                                             values_as_variable=['p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8'])))
+            )
+            for player in range(1, 9):
+                retrievers.append(
+                    Retriever(f"disabled_{disabled_type}_ids_player_{player}", DataType("u32"),
+                              on_refresh=RetrieverDependency(
+                                  DependencyAction.SET_REPEAT,
+                                  DependencyTarget("self", f"per_player_number_of_disabled_{disabled_type}s"),
+                                  DependencyEval(f"x[{player}]")),
+                              on_construct=RetrieverDependency(DependencyAction.REFRESH_SELF),
+                              on_commit=RetrieverDependency(
+                                  DependencyAction.REFRESH,
+                                  DependencyTarget("self", f"per_player_number_of_disabled_{disabled_type}s")
+                              ) if player == 1 else None)
+                )
+                # Unused: Players 9 - 16 can't have {disabled_type} technologies
+                Retriever(f"disabled_{disabled_type}_ids_player9-16", DataType("u32", repeat=0)),
+
+        retrievers += [
             Retriever("combat_mode", DataType("u32")),
             Retriever("naval_mode", DataType("u32")),
             Retriever("all_techs", DataType("u32")),
             Retriever("per_player_starting_age", DataType("u32", repeat=16)),  # 2: Dark 6 = Post | 1-8 players 9 GAIA
             Retriever("unknown", DataType("36")),
         ]
-
         super().__init__("Options", retrievers, parser_obj, data=data)
 
     @staticmethod
