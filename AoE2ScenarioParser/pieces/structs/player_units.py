f624d87f00279b87fdd1ef2e17b9aa51a7b7bbb1
KSneijders <k.sneijders@st.hanze.nl>
2020-10-28 00:11:18 +0100

    Added new Dependencies to player units piece

--- AoE2ScenarioParser/pieces/structs/player_units.py
+++ AoE2ScenarioParser/pieces/structs/player_units.py
@@ -1,5 +1,7 @@
 from AoE2ScenarioParser.helper.datatype import DataType
 from AoE2ScenarioParser.helper.retriever import Retriever
+from AoE2ScenarioParser.helper.retriever_dependency import RetrieverDependency, DependencyAction, DependencyTarget, \
+    DependencyEval
 from AoE2ScenarioParser.pieces.structs.aoe2_struct import AoE2Struct
 from AoE2ScenarioParser.pieces.structs.unit import UnitStruct
 
@@ -7,8 +9,16 @@ from AoE2ScenarioParser.pieces.structs.unit import UnitStruct
 class PlayerUnitsStruct(AoE2Struct):
     def __init__(self, parser_obj=None, data=None):
         retrievers = [
-            Retriever("unit_count", DataType("u32"), save_as="unit_count"),
-            Retriever("units", DataType(UnitStruct), set_repeat="{unit_count}")
+            Retriever("unit_count", DataType("u32"),
+                      on_refresh=RetrieverDependency(
+                          DependencyAction.SET_VALUE, DependencyTarget("self", "units"),
+                          DependencyEval("len(x)"))),
+            Retriever("units", DataType(UnitStruct),
+                      on_refresh=RetrieverDependency(
+                          DependencyAction.SET_REPEAT, DependencyTarget("self", "unit_count")),
+                      on_construct=RetrieverDependency(DependencyAction.REFRESH_SELF),
+                      on_commit=RetrieverDependency(
+                          DependencyAction.REFRESH, DependencyTarget("self", "unit_count")))
         ]
 
         super().__init__("Player Units", retrievers, parser_obj, data)
