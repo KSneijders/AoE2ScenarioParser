0b0ad9201c66c33b420967612cd8f2a006d13fae
KSneijders <k.sneijders@st.hanze.nl>
2020-11-23 22:01:28 +0100

    Changed the init with test objects

--- tests/test_object.py
+++ tests/test_object.py
@@ -8,20 +8,25 @@ class TestObject:
     file_name: str
     instance: TestObject
 
-    @classmethod
-    def get_instance(cls, scenario_folder="", file_name="default_scx"):
-        if not hasattr(TestObject, "instance"):
-            test_obj = cls()
-            test_obj.scenario_folder = scenario_folder
-            test_obj.file_name = file_name
-            TestObject.instance = test_obj
-            return test_obj
-        return TestObject.instance
-
     def init_files(self):
+        if not hasattr(self, "scenario_folder") or not hasattr(self, "file_name"):
+            raise ValueError("Path and filename bv")
         default_scenario = AoE2Scenario.create_default()
         read_scenario = AoE2Scenario.from_file(f"{self.scenario_folder}{self.file_name}.aoe2scenario")
         return {
             'default': default_scenario,
             'read': read_scenario
         }
+
+    @staticmethod
+    def get_instance():
+        if not hasattr(TestObject, "instance"):
+            return TestObject()
+        return TestObject.instance
+
+    @staticmethod
+    def init(scenario_folder="./", file_name="default_scx"):
+        test_obj = TestObject()
+        test_obj.scenario_folder = scenario_folder
+        test_obj.file_name = file_name
+        TestObject.instance = test_obj
