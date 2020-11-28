a9bd7eefe6dcfea302fe4a361c69c3616086972a
KSneijders <k.sneijders@st.hanze.nl>
2020-11-29 00:28:55 +0100

    Added AI2 Struct

---
+++ AoE2ScenarioParser/pieces/structs/ai2.py
@@ -0,0 +1,21 @@
+from AoE2ScenarioParser.helper.datatype import DataType
+from AoE2ScenarioParser.helper.retriever import Retriever
+from AoE2ScenarioParser.pieces.structs.aoe2_struct import AoE2Struct
+
+
+class AI2Struct(AoE2Struct):
+    def __init__(self, parser_obj=None, data=None, pieces=None):
+        retrievers = [
+            Retriever("ai_file_name", DataType("str32")),
+            Retriever("ai_file", DataType("str32")),
+        ]
+
+        super().__init__("AI2", retrievers, parser_obj, data, pieces=pieces)
+
+    @staticmethod
+    def defaults(pieces):
+        defaults = {
+            'ai_file_name': '',
+            'ai_file': '',
+        }
+        return defaults
