e96476e54b59947b06ceb276f046e41b8ab3ae11
KSneijders <k.sneijders@st.hanze.nl>
2020-11-29 01:52:02 +0100

    Improved triggers - Added AI file test

---
+++ tests/general.py
@@ -0,0 +1,10 @@
+from AoE2ScenarioParser.aoe2_scenario import AoE2Scenario
+
+
+def write_to_file(scx_type: str, scx: AoE2Scenario):
+    scx.write_to_file(f"./results/{scx_type}/test_{scx_type[0]}_no_edits.aoe2scenario")
+
+
+def read_ai_file(scx_name: str):
+    scx = AoE2Scenario.from_file(scx_name)
+    scx.write_to_file(f"./results/read/test_ai_file.aoe2scenario")
--- tests/test_object.py
+++ tests/test_object.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
+from typing import Tuple, List, Callable
+
 from AoE2ScenarioParser.aoe2_scenario import AoE2Scenario
+from tests import general
 
 
 class TestObject:
@@ -10,7 +13,7 @@ class TestObject:
 
     def init_files(self):
         if not hasattr(self, "scenario_folder") or not hasattr(self, "file_name"):
-            raise ValueError("Path and filename bv")
+            raise ValueError("Path and filename need to be initialised using TestObject.init()")
         default_scenario = AoE2Scenario.create_default()
         read_scenario = AoE2Scenario.from_file(f"{self.scenario_folder}{self.file_name}.aoe2scenario")
         return {
@@ -30,3 +33,38 @@ class TestObject:
         test_obj.scenario_folder = scenario_folder
         test_obj.file_name = file_name
         TestObject.instance = test_obj
+
+
+def print_section(name, section_length=90):
+    f_length = len(name)
+    half = int(section_length / 2) - int(f_length / 2) - 1
+    print("\n" + "\n".join([
+        "#" * section_length,
+        f"{'#' * half} {name} {'#' * (half - (1 if f_length % 2 == 1 else 0))}",
+        "#" * section_length,
+    ]))
+
+
+if __name__ == "__main__":
+    change_default_scenarios_tests = [
+        general.write_to_file
+    ]
+    other_scenarios_tests: List[Tuple[Callable, List[str]]] = [
+        (general.read_ai_file, ["ai_scx.aoe2scenario"])
+    ]
+
+    TestObject.init("./source/")
+
+    for test_function in change_default_scenarios_tests:
+        print_section(test_function.__name__)
+        scenarios = TestObject.get_instance().init_files()
+
+        for key, scenario in scenarios.items():
+            print_section(f"{test_function.__name__}->{key}", section_length=70)
+            test_function(key, scenario)
+
+    for function, scx_names in other_scenarios_tests:
+        print_section(f"{function.__name__}")
+        for name in scx_names:
+            print_section(f"{function.__name__}->{name}", section_length=70)
+            function(f"{TestObject.get_instance().scenario_folder}{name}")
