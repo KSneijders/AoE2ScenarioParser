67bf20cc7def1e6ee30d4ceda0d417da5ba5f9b3
KSneijders <k.sneijders@st.hanze.nl>
2021-01-17 02:44:20 +0100

    Added model_dict_from_structure to aoe2_struct_model.py

--- AoE2ScenarioParser/pieces/structs/aoe2_struct_model.py
+++ AoE2ScenarioParser/pieces/structs/aoe2_struct_model.py
@@ -1,21 +1,29 @@
-from typing import List
+from __future__ import annotations
 
-from AoE2ScenarioParser.helper.datatype import DataType
+from typing import List, Dict
+
 from AoE2ScenarioParser.helper.retriever import Retriever
 
 
 class AoE2StructModel:
-    def __init__(self, name: str, retrievers: List[Retriever]):
+    def __init__(self, name: str, retrievers: List[Retriever], structs: Dict[AoE2StructModel]):
         self.name = name
         self.retrievers = retrievers
+        self.structs = structs
 
     @classmethod
     def from_structure(cls, name, structure) -> AoE2StructModel:
         retrievers = []
         for retriever_name, attr in structure.get('retrievers', {}).items():
-            datatype = DataType(var=attr.get('type'), repeat=attr.get('repeat', 1))
-            retrievers.append(Retriever(
-                name=retriever_name,
-                datatype=datatype
-            ))
-        return cls(name, retrievers)
+            retrievers.append(Retriever.from_structure(retriever_name, attr))
+        structs = model_dict_from_structure(structure)
+
+        return cls(name, retrievers, structs)
+
+
+def model_dict_from_structure(structure) -> Dict[AoE2StructModel]:
+    models = {}
+    for name, attr in structure.get('structs', {}).items():
+        # Create struct model
+        models[name] = AoE2StructModel.from_structure(name, attr)
+    return models
