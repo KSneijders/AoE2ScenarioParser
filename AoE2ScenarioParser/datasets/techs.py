98084fc18cf7ae32d9b7ab00ee6568dfe4e6a658
KSneijders <k.sneijders@st.hanze.nl>
2021-04-05 17:14:19 +0200

    Added new functions to tech dataset

--- AoE2ScenarioParser/datasets/techs.py
+++ AoE2ScenarioParser/datasets/techs.py
@@ -28,6 +28,27 @@ class TechInfo(Enum):
                 return x
         raise ValueError(f"{value} is not a valid icon_id value")
 
+    @staticmethod
+    def unique_units():
+        return [
+            TechInfo.YEOMEN, TechInfo.EL_DORADO, TechInfo.FUROR_CELTICA, TechInfo.DRILL, TechInfo.MAHOUTS,
+            TechInfo.ZEALOTRY, TechInfo.ARTILLERY, TechInfo.CRENELLATIONS, TechInfo.ANARCHY, TechInfo.ATHEISM,
+            TechInfo.GARLAND_WARS, TechInfo.BERSERKERGANG, TechInfo.ROCKETRY, TechInfo.KATAPARUTO, TechInfo.LOGISTICA,
+            TechInfo.BEARDED_AXE, TechInfo.SUPREMACY, TechInfo.SHINKICHON, TechInfo.PERFUSION, TechInfo.ATLATL,
+            TechInfo.WARWOLF, TechInfo.GREAT_WALL, TechInfo.CHIEFTAINS, TechInfo.GREEK_FIRE, TechInfo.STRONGHOLD,
+            TechInfo.MARAUDERS, TechInfo.YASAMA, TechInfo.OBSIDIAN_ARROWS, TechInfo.PANOKSEON, TechInfo.NOMADS,
+            TechInfo.KAMANDARAN, TechInfo.IRONCLAD, TechInfo.MADRASAH, TechInfo.SIPAHI, TechInfo.INQUISITION,
+            TechInfo.CHIVALRY, TechInfo.PAVISE, TechInfo.SILK_ROAD, TechInfo.SULTANS, TechInfo.SHATAGNI,
+            TechInfo.ORTHODOXY, TechInfo.DRUZHINA, TechInfo.CORVINIAN_ARMY, TechInfo.RECURVE_BOW, TechInfo.ANDEAN_SLING,
+            TechInfo.COURIERS, TechInfo.CARRACK, TechInfo.ARQUEBUS, TechInfo.ROYAL_HEIRS, TechInfo.TORSION_ENGINES,
+            TechInfo.TIGUI, TechInfo.FARIMBA, TechInfo.KASBAH, TechInfo.MAGHRABI_CAMELS, TechInfo.TUSK_SWORDS,
+            TechInfo.DOUBLE_CROSSBOW, TechInfo.THALASSOCRACY, TechInfo.FORCED_LEVY, TechInfo.HOWDAH,
+            TechInfo.MANIPUR_CAVALRY, TechInfo.CHATRAS, TechInfo.PAPER_MONEY, TechInfo.STIRRUPS, TechInfo.BAGAINS,
+            TechInfo.SILK_ARMOR, TechInfo.TIMURID_SIEGECRAFT, TechInfo.STEPPE_HUSBANDRY, TechInfo.CUMAN_MERCENARIES,
+            TechInfo.HILL_FORTS, TechInfo.TOWER_SHIELDS, TechInfo.BURGUNDIAN_VINEYARDS, TechInfo.FELMISH_REVOLUTION,
+            TechInfo.FIRST_CRUSADE, TechInfo.SCUTAGE,
+        ]
+
     ANARCHY = 16, 33
     ANDEAN_SLING = 516, 33
     ARBALESTER = 237, 54
