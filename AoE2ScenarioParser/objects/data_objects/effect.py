f293038ca786220bc8e7a4709977d4a0a7099852
KSneijders <k.sneijders@st.hanze.nl>
2021-09-16 18:15:29 +0200

    Allow effect strings to be set to bytes

--- AoE2ScenarioParser/objects/data_objects/effect.py
+++ AoE2ScenarioParser/objects/data_objects/effect.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import List, Tuple
+from typing import List, Tuple, Union
 
 from AoE2ScenarioParser.datasets import effects
 from AoE2ScenarioParser.datasets.effects import EffectId
@@ -16,9 +16,9 @@ from AoE2ScenarioParser.sections.retrievers.retriever_object_link import Retriev
 from AoE2ScenarioParser.sections.retrievers.support import Support
 
 
-def _add_trail_if_string_attr_is_used_in_effect(obj: Effect, attr_name, val):
+def _add_trail_if_string_attr_is_used_in_effect(obj: Effect, attr_name, val: Union[bytes, str]):
     if attr_name in effects.attributes[obj.effect_type]:
-        return val + "\x00"
+        return val + (b"\x00" if type(val) is bytes else "\x00")
     return val
 
 
