69e8113faaef108118495ca38906325fd092505d
KSneijders <k.sneijders@st.hanze.nl>
2021-09-22 16:00:57 +0200

    Changed minimum requirement of python to 3.8

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     install_requires=[
         'deprecation'
     ]
