eac63bb553d37e519170c10d0196be382b818254
Alian713 <divy1211.dc@gmail.com>
2021-12-14 04:05:22 +0800

    added unstaged file

--- AoE2ScenarioParser/scenarios/scenario_store/store.py
+++ AoE2ScenarioParser/scenarios/scenario_store/store.py
@@ -4,15 +4,15 @@ if TYPE_CHECKING:
     from AoE2ScenarioParser.scenarios.aoe2_scenario import AoE2Scenario
     from AoE2ScenarioParser.scenarios.aoe2_de_scenario import AoE2DEScenario
 
-_scenarios: Dict[str, AoE2Scenario] = {}
+_scenarios: Dict[str, 'AoE2Scenario'] = {}
 
 
-def get_scenario(uuid: str) -> Optional[AoE2Scenario]:
+def get_scenario(uuid: str) -> Optional['AoE2Scenario']:
     """
     Get scenario through uuid. Not intended to be called outside of the store itself.
 
     Args:
-        uuid (UUID): The universally unique identifier of the scenario
+        uuid: The universally unique identifier of the scenario
 
     Returns:
         The scenario based on it's uuid
@@ -22,12 +22,12 @@ def get_scenario(uuid: str) -> Optional[AoE2Scenario]:
     return _scenarios[uuid]
 
 
-def register_scenario(scenario: AoE2Scenario) -> None:
+def register_scenario(scenario: 'AoE2Scenario') -> None:
     """
     Register a scenario to the store
 
     Args:
-        scenario (AoE2DEScenario): The scenario to register
+        scenario: The scenario to register
     """
     if scenario.uuid in _scenarios:
         raise ValueError("Scenario with that UUID already present")
