469f96c1a55131aeac91157136df4c5bb968bcc1
KSneijders <k.sneijders@st.hanze.nl>
2022-01-07 19:04:43 +0100

    Added triggers from trigger markings

--- AoE2ScenarioParser/scenarios/support/trigger_markings.py
+++ AoE2ScenarioParser/scenarios/support/trigger_markings.py
@@ -6,7 +6,6 @@ from AoE2ScenarioParser.helper.helper import values_are_valid, value_is_valid
 from AoE2ScenarioParser.objects.data_objects.condition import Condition
 from AoE2ScenarioParser.objects.data_objects.effect import Effect
 from AoE2ScenarioParser.objects.data_objects.trigger import Trigger
-from AoE2ScenarioParser.objects.data_objects.unit import Unit
 from AoE2ScenarioParser.objects.support.area import Area
 from AoE2ScenarioParser.objects.support.tile import Tile
 from AoE2ScenarioParser.scenarios.scenario_store import getters, actions
@@ -16,8 +15,8 @@ class TriggerMarkings:
     _scan_options: Dict[str, List[str]] = {
         "area": ["area", "areas"],
         "tile": ["tile", "tiles"],
-        "location": ["location", "locations"],
         "object": ["object", "objects"],
+        "trigger": ["trigger", "triggers"],
     }
 
     def __init__(self, uuid: UUID) -> None:
@@ -28,6 +27,7 @@ class TriggerMarkings:
         self.objects = AttrDict()
         self.areas = AttrDict()
         self.tiles = AttrDict()
+        self.triggers = AttrDict()
 
     def discover(self, remove_template_triggers: bool) -> None:
         triggers = getters.get_triggers_by_prefix(
@@ -40,23 +40,29 @@ class TriggerMarkings:
             tag = self._resolve_markings_name(trigger)
             for ce in loop_trigger_content(trigger):
                 if trigger.name.startswith("area"):
-                    area = self._create_area(ce)
-                    if area:
+                    if area := self._create_area(ce):
                         self.areas.setdefault(tag, []).append(area)
 
-                elif trigger.name.startswith(("tile", "location")):
-                    tiles = self._create_tiles(ce)
-                    if tiles:
+                elif trigger.name.startswith("tile"):
+                    if tiles := self._create_tiles(ce):
                         self.tiles.setdefault(tag, []).extend(tiles)
 
                 elif trigger.name.startswith("object"):
                     object_ids.extend(self._get_unit_ids(ce))
+
+                elif trigger.name.startswith("trigger"):
+                    if found_trigger := self._get_trigger(ce):
+                        self.triggers.setdefault(tag, []).append(found_trigger)
 
             if object_ids:
                 self.objects.setdefault(tag, []).extend(getters.get_units(self._uuid, object_ids)[0])
 
         if remove_template_triggers:
             actions.remove_triggers(self._uuid, [t.trigger_id for t in triggers])
+
+    def _get_trigger(self, ce: Union['Condition', 'Effect']) -> Optional[Trigger]:
+        if value_is_valid(ce.trigger_id):
+            return getters.get_trigger(self._uuid, ce.trigger_id)
 
     def _create_area(self, ce: Union['Condition', 'Effect']) -> Optional[Area]:
         if values_are_valid(ce.area_x1, ce.area_y1, ce.area_x2, ce.area_y2):
