65d5d3bf4b05f5cbb6d856afe12664c4705b0ceb
KSneijders <k.sneijders@st.hanze.nl>
2022-01-24 20:26:28 +0100

    Added type annotation possibilities to AttrDict

--- AoE2ScenarioParser/helper/attr_dict.py
+++ AoE2ScenarioParser/helper/attr_dict.py
@@ -1,4 +1,10 @@
-class AttrDict(dict):
+import typing
+
+_KT = typing.TypeVar("_KT")  # Key type
+_VT = typing.TypeVar("_VT")  # Value type
+
+
+class AttrDict(dict, typing.Mapping[_KT, _VT]):
     def __init__(self, *args):
         dict.__init__(self, args)
 
@@ -6,7 +12,10 @@ class AttrDict(dict):
         try:
             return self[item]
         except (AttributeError, KeyError):
-            return dict.__getattribute__(self, item)
+            try:
+                return dict.__getattribute__(self, item)
+            except AttributeError:
+                return None
 
     def __setattr__(self, key, value):
         self[key] = value
