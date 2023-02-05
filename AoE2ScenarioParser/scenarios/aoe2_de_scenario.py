95b28a9386f0ba6420b87bb072a6da1daf3b60ed
KSneijders <ksneijders@hotmail.com>
2023-02-05 22:57:02 +0100

    Added warning message when doing old import

---
+++ AoE2ScenarioParser/scenarios/aoe2_de_scenario.py
@@ -0,0 +1,27 @@
+
+raise Exception('\n'.join([
+    "",
+    "",
+    "###################################",
+    "# PLEASE READ THE STATEMENT BELOW #",
+    "###################################",
+    "",
+    "You're code is likely not updated for AoE2ScenarioParser v1.0.0!",
+    "",
+    "AoE2ScenarioParser has updated to the first major version, version 1.0.0!",
+    "This update brings a lot of changes which should help the development experience A LOT.",
+    "",
+    "Unfortunately, this does mean that if you want to use v1.0.0 you'll have to update your code.",
+    "A document that lists all changes that you need to make to your code can be found here:",
+    ">>> https://ksneijders.github.io/.../UPDATE_GUIDE_URL/",  # TODO: <-- ADD URL TO UPDATE GUIDE
+    "",
+    "For any questions regarding the above linked document or questions in general please refer to the discord server:",
+    ">>> https://discord.gg/DRUtmugXT3",
+    "",
+    "In case you do NOT want to use v1.0.0+ you can downgrade your version using:",
+    ">>> `pip install 'AoE2ScenarioParser==<VERSION>' --force-reinstall`",  # TODO: <-- ADD LAST VERSION BEFORE v1.0.0
+    "",
+    "Please do note the following if you decide to downgrade:",
+    "Newer versions of Age of Empires: Definitive Edition will not be supported by older versions.",
+    "",
+]))
