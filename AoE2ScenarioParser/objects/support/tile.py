91deee221892c124d2193986a9010821ca54f5b4
Alian713 <divy1211.dc@gmail.com>
2023-06-09 17:25:37 +1000

    Revert "Converted Tile to Struct"
    This reverts commit 004a2630fc68cc85ebe1e47b7d0569e335995cd4.

--- AoE2ScenarioParser/objects/support/tile.py
+++ AoE2ScenarioParser/objects/support/tile.py
@@ -3,27 +3,20 @@ from __future__ import annotations
 import math
 from typing import TYPE_CHECKING, Iterable, overload
 
-from binary_file_parser import RetrieverRef, Version
-
 from AoE2ScenarioParser.helper.coordinates import i_to_xy, xy_to_i
 from AoE2ScenarioParser.helper.helper import bound
-from AoE2ScenarioParser.sections.bfp.triggers import TileStruct
 
 if TYPE_CHECKING:
     from AoE2ScenarioParser.objects.support import TileT
 
 
-class Tile(TileStruct):
-    x = RetrieverRef(TileStruct._x)
-    y = RetrieverRef(TileStruct._y)
-
+class Tile:
     @overload
     def __init__(self, x: int, y: int): ...
     @overload
     def __init__(self, tile: tuple[int, int]): ...
 
-    def __init__(self, x: int | tuple[int, int], y: int | None = None, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, x: int | tuple[int, int], y: int | None = None):
         if isinstance(x, tuple):
             if len(x) != 2:
                 raise ValueError("tuple to initialise a Tile must have exactly two coordinates")
--- AoE2ScenarioParser/sections/bfp/triggers/__init__.py
+++ AoE2ScenarioParser/sections/bfp/triggers/__init__.py
@@ -1,2 +1,1 @@
 from .effect_struct import EffectStruct
-from .tile_struct import TileStruct
