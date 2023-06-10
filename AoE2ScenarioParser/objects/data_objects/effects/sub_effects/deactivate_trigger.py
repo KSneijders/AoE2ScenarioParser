8232eaa2e595c4026911eccb439a12f6c8921651
KSneijders <ksneijders-dev@hotmail.com>
2023-06-10 16:39:09 +0200

    Added (de)activate trigger effects

---
+++ AoE2ScenarioParser/objects/data_objects/effects/sub_effects/deactivate_trigger.py
@@ -0,0 +1,32 @@
+from typing import overload
+
+from binary_file_parser.retrievers import RetrieverRef
+
+from AoE2ScenarioParser.datasets.triggers import EffectType
+from AoE2ScenarioParser.objects.data_objects.effects.effect import Effect
+from AoE2ScenarioParser.sections.bfp.triggers import EffectStruct
+
+
+class DeactivateTrigger(Effect):
+    trigger: int = RetrieverRef(EffectStruct._trigger_id)  # type:ignore
+    """The trigger to deactivate"""
+
+    @overload
+    def __init__(self, trigger: int): ...
+
+    def __init__(
+        self,
+        trigger: list[int],
+        **kwargs,
+    ):
+        """
+        Play a sound for the source_player
+
+        Args:
+            trigger: The trigger to deactivate
+        """
+        super().__init__(local_vars = locals(), **kwargs)
+
+    @property
+    def type(self) -> EffectType:
+        return EffectType.DEACTIVATE_TRIGGER
