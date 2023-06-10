9d85a51cade529fb9d4031352207130c0bde081f
KSneijders <ksneijders-dev@hotmail.com>
2023-06-10 16:52:38 +0200

    Added ai script goal effect

---
+++ AoE2ScenarioParser/objects/data_objects/effects/sub_effects/ai_script_goal.py
@@ -0,0 +1,34 @@
+from typing import overload
+
+from binary_file_parser.retrievers import RetrieverRef
+
+from AoE2ScenarioParser.datasets.triggers import EffectType
+from AoE2ScenarioParser.objects.data_objects.effects.effect import Effect
+from AoE2ScenarioParser.sections.bfp.triggers import EffectStruct
+
+
+class AiScriptGoal(Effect):
+    ai_trigger_id: int = RetrieverRef(EffectStruct._ai_script_goal)  # type:ignore
+    """The AI Trigger ID to trigger"""
+
+    @overload
+    def __init__(self, ai_trigger_id: int): ...
+
+    def __init__(
+        self,
+        ai_trigger_id: int,
+        **kwargs,
+    ):
+        """
+        Used to communicate with an AI.
+        Calling this effect with ai_trigger_id set to (for example) 4 can be detected in an AI script
+        using ``event-detected trigger 4``.
+
+        Args:
+            ai_trigger_id: The AI Trigger ID to trigger
+        """
+        super().__init__(local_vars = locals(), **kwargs)
+
+    @property
+    def type(self) -> EffectType:
+        return EffectType.AI_SCRIPT_GOAL
