fcff5be4fad815aa257f36618e0534ef431f3f11
KSneijders <ksneijders-dev@hotmail.com>
2023-06-10 16:36:53 +0200

    Added lock gate effect

---
+++ AoE2ScenarioParser/objects/data_objects/effects/sub_effects/lock_gate.py
@@ -0,0 +1,38 @@
+from typing import overload
+
+from binary_file_parser.retrievers import RetrieverRef
+
+from AoE2ScenarioParser.datasets.triggers import EffectType
+from AoE2ScenarioParser.helper.list_functions import listify
+from AoE2ScenarioParser.objects.data_objects.effects.effect import Effect
+from AoE2ScenarioParser.sections.bfp.triggers import EffectStruct
+
+
+class LockGate(Effect):
+    gates: list[int] = RetrieverRef(EffectStruct._selected_object_ids)  # type:ignore
+    """The gate(s) to lock"""
+
+    @overload
+    def __init__(self, gates: list[int]): ...
+
+    @overload
+    def __init__(self, gates: int): ...
+
+    def __init__(
+        self,
+        gates: list[int],
+        **kwargs,
+    ):
+        """
+        Play a sound for the source_player
+
+        Args:
+            gates: The gate(s) to lock
+        """
+        gates = listify(gates)
+
+        super().__init__(local_vars = locals(), **kwargs)
+
+    @property
+    def type(self) -> EffectType:
+        return EffectType.LOCK_GATE
