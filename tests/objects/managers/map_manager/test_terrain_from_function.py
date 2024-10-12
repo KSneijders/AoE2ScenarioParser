7554790070431763827740bd6569762c6ca66c8c
KSneijders <ksneijders-dev@hotmail.com>
2024-10-12 12:48:06 +0200

    Converted MapManager test_terrain_from to pytest

--- tests/objects/managers/map_manager/test_terrain_from_function.py
+++ tests/objects/managers/map_manager/test_terrain_from_function.py
@@ -1,28 +1,29 @@
 from unittest import TestCase
 
+import pytest
+
 from AoE2ScenarioParser.managers import MapManager
 from AoE2ScenarioParser.objects.support import Tile
-from objects.managers.map_manager import MockScxSecTestMM
+from objects.managers.map_manager import MockMapManager
 
 
-class Test(TestCase):
-    mm: MapManager
+@pytest.fixture
+def mm():
+    return MapManager(MockMapManager())
 
-    def setUp(self) -> None:
-        self.mm = MapManager(MockScxSecTestMM())
 
-    def test_terrain_from_function_tile_args(self):
-        terrain_3_3 = self.mm.get_tile(Tile(3, 3))
-        terrain_1_1 = self.mm.get_tile(Tile(1, 1))
-        terrain_4_3 = self.mm.get_tile(Tile(4, 3))
+def test_terrain_from_function_tile_args(mm: MapManager):
+    terrain_3_3 = mm.get_tile(Tile(3, 3))
+    terrain_1_1 = mm.get_tile(Tile(1, 1))
+    terrain_4_3 = mm.get_tile(Tile(4, 3))
 
-        tiles = self.mm.terrain_from(Tile(3, 3))
-        self.assertEqual(len(tiles), 1)
-        self.assertEqual(list(tiles.keys()), [Tile(3, 3)])
-        self.assertIs(tiles[Tile(3, 3)], terrain_3_3)
+    tiles = mm.terrain_from(Tile(3, 3))
+    assert len(tiles) == 1
+    assert list(tiles.keys()) == [Tile(3, 3)]
+    assert tiles[Tile(3, 3)] == terrain_3_3
 
-        tiles = self.mm.terrain_from((Tile(1, 1), Tile(4, 3)))
-        self.assertEqual(len(tiles), 2)
-        self.assertEqual(list(tiles.keys()), [Tile(1, 1), Tile(4, 3)])
-        self.assertIs(tiles[Tile(1, 1)], terrain_1_1)
-        self.assertIs(tiles[Tile(4, 3)], terrain_4_3)
+    tiles = mm.terrain_from((Tile(1, 1), Tile(4, 3)))
+    assert len(tiles) == 2
+    assert list(tiles.keys()) == [Tile(1, 1), Tile(4, 3)]
+    assert tiles[Tile(1, 1)] == terrain_1_1
+    assert tiles[Tile(4, 3)] == terrain_4_3
