d218b467e6caec12ae73e606b34b276edd170004
KSneijders <ksneijders-dev@hotmail.com>
2024-10-12 13:20:53 +0200

    Converted Tile tests to pytest

--- tests/objects/support/test_tile.py
+++ tests/objects/support/test_tile.py
@@ -1,97 +1,107 @@
 import math
-from unittest import TestCase
+
+import pytest
 
 from AoE2ScenarioParser.objects.support import Tile
 
 
-class TestTile(TestCase):
-    tile: Tile
+@pytest.fixture
+def tile():
+    return Tile(3, 4)
 
-    def setUp(self) -> None:
-        self.tile = Tile(3, 4)
 
-    def test_from_i(self):
-        self.tile = Tile.from_i(2, 10)
-        self.assertEqual(2, self.tile.x)
-        self.assertEqual(0, self.tile.y)
+def test_from_i(tile):
+    tile = Tile.from_i(2, 10)
+    assert tile.x == 2
+    assert tile.y == 0
 
-        self.tile = Tile.from_i(10, 10)
-        self.assertEqual(0, self.tile.x)
-        self.assertEqual(1, self.tile.y)
+    tile = Tile.from_i(10, 10)
+    assert tile.x == 0
+    assert tile.y == 1
 
-        self.tile = Tile.from_i(10, 8)
-        self.assertEqual(2, self.tile.x)
-        self.assertEqual(1, self.tile.y)
+    tile = Tile.from_i(10, 8)
+    assert tile.x == 2
+    assert tile.y == 1
 
-        self.assertRaises(ValueError, lambda: Tile.from_i(102, 10))
+    with pytest.raises(ValueError):
+        Tile.from_i(102, 10)
 
-    def test_to_i(self):
-        self.tile = Tile.from_i(2, 10)
-        self.assertEqual(2, self.tile.to_i(10))
 
-        self.tile = Tile.from_i(10, 10)
-        self.assertEqual(10, self.tile.to_i(10))
+def test_to_i(tile):
+    tile = Tile.from_i(2, 10)
+    assert tile.to_i(10) == 2
 
-        self.tile = Tile.from_i(10, 8)
-        self.assertEqual(10, self.tile.to_i(8))
+    tile = Tile.from_i(10, 10)
+    assert tile.to_i(10) == 10
 
-    def test_bound(self):
-        self.assertEqual(self.tile, self.tile.bound(5))
-        self.assertEqual(Tile(2, 2), self.tile.bound(2))
+    tile = Tile.from_i(10, 8)
+    assert tile.to_i(8) == 10
 
-        self.tile = Tile(10, 4)
-        self.assertEqual(Tile(6, 4), self.tile.bound(6))
 
-    def test_resolve_negative_coords(self):
-        self.tile = Tile(-1, -1)
-        self.assertEqual(Tile(9, 9), self.tile.resolve_negative_coords(10))
-        self.tile = Tile(-1, 5)
-        self.assertEqual(Tile(9, 5), self.tile.resolve_negative_coords(10))
+def test_bound(tile):
+    assert tile == tile.bound(5)
+    assert Tile(2, 2) == tile.bound(2)
 
-    def test_mid_tile(self):
-        self.assertEqual(self.tile, self.tile.mid_tile(self.tile))
+    tile = Tile(10, 4)
+    assert Tile(6, 4) == tile.bound(6)
 
-        self.assertEqual(Tile(3, 5), self.tile.mid_tile(Tile(3, 6)))
-        self.assertEqual(Tile(5, 7), self.tile.mid_tile(Tile(7, 9)))
-        self.assertEqual(Tile(4, 3), self.tile.mid_tile(Tile(5, 2)))
-        self.assertEqual(Tile(2, 2), self.tile.mid_tile(Tile(0, 0)))
 
-        self.assertEqual(Tile(3, 5), self.tile.mid_tile(Tile(3, 7), de_behaviour=False))
-        self.assertEqual(Tile(3, 6), self.tile.mid_tile(Tile(3, 7), de_behaviour=True))
+def test_resolve_negative_coords(tile):
+    tile = Tile(-1, -1)
+    assert Tile(9, 9) == tile.resolve_negative_coords(10)
+    tile = Tile(-1, 5)
+    assert Tile(9, 5) == tile.resolve_negative_coords(10)
 
-    def test_mid_point(self):
-        self.assertEqual((1.5, 2.0), self.tile.mid_point(Tile(0, 0)))
-        self.assertEqual((3.0, 5.0), self.tile.mid_point(Tile(3, 6)))
-        self.assertEqual((4.5, 3.5), self.tile.mid_point(Tile(6, 3)))
 
-    def test_dist(self):
-        self.assertEqual(0.0, self.tile.dist(Tile(3, 4)))
-        self.assertEqual(1.0, self.tile.dist(Tile(4, 4)))
-        self.assertEqual(1.0, self.tile.dist(Tile(3, 3)))
-        self.assertEqual(math.sqrt(2), self.tile.dist(Tile(2, 3)))
+def test_mid_tile(tile):
+    assert tile == tile.mid_tile(tile)
 
-    def test_dist_taxicab(self):
-        self.assertEqual(0.0, self.tile.dist_taxicab(Tile(3, 4)))
-        self.assertEqual(1.0, self.tile.dist_taxicab(Tile(4, 4)))
-        self.assertEqual(1.0, self.tile.dist_taxicab(Tile(3, 3)))
-        self.assertEqual(2.0, self.tile.dist_taxicab(Tile(2, 3)))
+    assert Tile(3, 5) == tile.mid_tile(Tile(3, 6))
+    assert Tile(5, 7) == tile.mid_tile(Tile(7, 9))
+    assert Tile(4, 3) == tile.mid_tile(Tile(5, 2))
+    assert Tile(2, 2) == tile.mid_tile(Tile(0, 0))
 
-    def test_from_value(self):
-        tile = Tile(34, 4)
+    assert Tile(3, 5) == tile.mid_tile(Tile(3, 7), de_behaviour = False)
+    assert Tile(3, 6) == tile.mid_tile(Tile(3, 7), de_behaviour = True)
 
-        self.assertIs(tile, Tile.from_value(tile))
-        self.assertEqual(Tile(1, 2), Tile.from_value((1, 2)))
-        self.assertEqual(Tile(1, 2), Tile.from_value([1, 2]))
-        self.assertEqual(Tile(1, 2), Tile.from_value({'x': 1, 'y': 2}))
 
-    def test_is_within_bounds(self):
-        tile = Tile(34, 4)
+def test_mid_point(tile):
+    assert tile.mid_point(Tile(0, 0)) == (1.5, 2.0)
+    assert tile.mid_point(Tile(3, 6)) == (3.0, 5.0)
+    assert tile.mid_point(Tile(6, 3)) == (4.5, 3.5)
 
-        self.assertTrue(tile.is_within_bounds(50))
-        self.assertTrue(tile.is_within_bounds(35))
-        self.assertFalse(tile.is_within_bounds(34))
 
-        tile = Tile(-1, -20)
-        self.assertTrue(tile.is_within_bounds(50))
-        self.assertTrue(tile.is_within_bounds(35))
-        self.assertFalse(tile.is_within_bounds(19))
+def test_dist(tile):
+    assert tile.dist(Tile(3, 4)) == 0.0
+    assert tile.dist(Tile(4, 4)) == 1.0
+    assert tile.dist(Tile(3, 3)) == 1.0
+    assert tile.dist(Tile(2, 3)) == math.sqrt(2)
+
+
+def test_dist_taxicab(tile):
+    assert tile.dist_taxicab(Tile(3, 4)) == 0.0
+    assert tile.dist_taxicab(Tile(4, 4)) == 1.0
+    assert tile.dist_taxicab(Tile(3, 3)) == 1.0
+    assert tile.dist_taxicab(Tile(2, 3)) == 2.0
+
+
+def test_from_value():
+    tile = Tile(34, 4)
+
+    assert tile is Tile.from_value(tile)
+    assert Tile(1, 2) == Tile.from_value((1, 2))
+    assert Tile(1, 2) == Tile.from_value([1, 2])
+    assert Tile(1, 2) == Tile.from_value({'x': 1, 'y': 2})
+
+
+def test_is_within_bounds():
+    tile = Tile(34, 4)
+
+    assert tile.is_within_bounds(50) is True
+    assert tile.is_within_bounds(35) is True
+    assert tile.is_within_bounds(34) is False
+
+    tile = Tile(-1, -20)
+    assert tile.is_within_bounds(50) is True
+    assert tile.is_within_bounds(35) is True
+    assert tile.is_within_bounds(19) is False
