19ba94a6fdf87d24f9443943433353c31e4842d0
KSneijders <ksneijders-dev@hotmail.com>
2024-10-12 12:48:19 +0200

    Converted Area tests to pytest

--- tests/objects/support/test_area.py
+++ tests/objects/support/test_area.py
@@ -1,99 +1,108 @@
-from unittest import TestCase
+import pytest
 
 from AoE2ScenarioParser.objects.support import Area, Tile
 
 
-class TestArea(TestCase):
-    area: Area
+@pytest.fixture
+def area() -> Area:
+    return Area((2, 2), (5, 6))
 
-    def setUp(self) -> None:
-        self.area = Area((2, 2), (5, 6))
 
-    def test_init(self):
-        self.assertEqual(2, self.area.corner1.x)
-        self.assertEqual(2, self.area.corner1.y)
-        self.assertEqual(5, self.area.corner2.x)
-        self.assertEqual(6, self.area.corner2.y)
+def test_init(area: Area):
+    assert 2 == area.corner1.x
+    assert 2 == area.corner1.y
+    assert 5 == area.corner2.x
+    assert 6 == area.corner2.y
 
-        self.assertEqual(self.area, Area((2, 2), (5, 6)))
-        self.assertEqual(Area((2, 3), (2, 3)), Area((2, 3)))
+    assert area == Area((2, 2), (5, 6))
+    assert Area((2, 3), (2, 3)) == Area((2, 3))
 
-        self.assertEqual(Area((2, 2), (4, 4)), Area((4, 4), (2, 2)))
+    assert Area((2, 2), (4, 4)) == Area((4, 4), (2, 2))
 
-    def test_center(self):
-        """Short test as the specific logic is tested within the Tile class"""
-        self.assertEqual(Tile(4, 4), self.area.center_tile)
 
-    def test_center_point(self):
-        """Short test as the specific logic is tested within the Tile class"""
-        self.assertEqual((3.5, 4.0), self.area.center_point)
+def test_center(area: Area):
+    """Short test as the specific logic is tested within the Tile class"""
+    assert Tile(4, 4) == area.center_tile
 
-    def test_width(self):
-        self.assertEqual(4.0, self.area.width)
 
-    def test_height(self):
-        self.assertEqual(5.0, self.area.height)
+def test_center_point(area: Area):
+    """Short test as the specific logic is tested within the Tile class"""
+    assert (3.5, 4.0) == area.center_point
 
-    def test_dimensions(self):
-        self.assertEqual((4.0, 5.0), self.area.dimensions)
 
-    def test_resolve_negative_coords(self):
-        self.area = Area((-4, -4), (-2, -2))
-        self.assertEqual(Area((6, 6), (8, 8)), self.area.resolve_negative_coords(10))
-        # self.area = Tile(-1, 5)
-        # self.assertEqual(Tile(9, 5), self.area.resolve_negative_coords(10))
+def test_width(area: Area):
+    assert 4.0 == area.width
 
-    def test_bound(self):
-        self.assertEqual(Area((2, 2), (4, 4)), self.area.bound(4))
-        self.assertEqual(Area((2, 2), (3, 4)), Area((2, 2), (3, 6)).bound(4))
-        self.assertEqual(Area((0, 0)), Area((-1, -1)).bound(4))
 
-    def test_contains(self):
-        self.assertTrue(self.area.contains(Tile(2, 2)))
-        self.assertTrue(self.area.contains(Tile(2, 6)))
-        self.assertTrue(self.area.contains(Tile(5, 2)))
-        self.assertTrue(self.area.contains(Tile(5, 6)))
+def test_height(area: Area):
+    assert 5.0 == area.height
 
-        self.assertTrue(self.area.contains(Tile(4, 2)))
-        self.assertTrue(self.area.contains(Tile(3, 3)))
 
-        self.assertFalse(self.area.contains(Tile(2, 1)))
-        self.assertFalse(self.area.contains(Tile(3, 7)))
-        self.assertFalse(self.area.contains(Tile(6, 2)))
+def test_dimensions(area: Area):
+    assert (4.0, 5.0) == area.dimensions
 
-    def test__iter__(self):
-        tiles = (self.area.corner1, self.area.corner2)
-        for i, t in enumerate(self.area):
-            self.assertIs(tiles[i], t)
 
-    def test__getitem__(self):
-        tiles = (self.area.corner1, self.area.corner2)
-        for i in range(2):
-            self.assertTrue(tiles[i], self.area[i])
+def test_resolve_negative_coords(area: Area):
+    area = Area((-4, -4), (-2, -2))
+    assert Area((6, 6), (8, 8)) == area.resolve_negative_coords(10)
 
-    def test__eq__(self):
-        self.assertEqual(self.area, Area((2, 2), (5, 6)))
-        self.assertNotEqual(self.area, Area((2, 3), (5, 6)))
 
-        self.assertEqual(Area((3, 3)), Area((3, 3), (3, 3)))
-        self.assertEqual(((3, 4), (5, 6)), Area((3, 4), (5, 6)))
+def test_bound(area: Area):
+    assert Area((2, 2), (4, 4)) == area.bound(4)
+    assert Area((2, 2), (3, 4)) == Area((2, 2), (3, 6)).bound(4)
+    assert Area((0, 0)) == Area((-1, -1)).bound(4)
 
-    def test_from_value(self):
-        area = Area((0, 1), (0, 1))
 
-        self.assertEqual(area, Area.from_value(Tile(0, 1)))
-        self.assertEqual(area, Area.from_value((0, 1)))
-        self.assertEqual(area, Area.from_value([0, 1]))
-        self.assertEqual(area, Area.from_value({'a': 0, 'b': 1}))
-        self.assertEqual(area, Area.from_value({'corner1': Tile(0, 1)}))
+def test_contains(area: Area):
+    assert area.contains(Tile(2, 2))
+    assert area.contains(Tile(2, 6))
+    assert area.contains(Tile(5, 2))
+    assert area.contains(Tile(5, 6))
 
-        area = Area((0, 1), (2, 3))
-        self.assertEqual(area, Area.from_value(area))
-        self.assertEqual(area, Area.from_value((Tile(0, 1), Tile(2, 3))))
-        self.assertEqual(area, Area.from_value((0, 1, 2, 3)))
-        self.assertEqual(area, Area.from_value([0, 1, 2, 3]))
-        self.assertEqual(area, Area.from_value([Tile(0, 1), Tile(2, 3)]))
-        self.assertEqual(area, Area.from_value([Tile(0, 1), Tile(2, 3)]))
-        self.assertEqual(area, Area.from_value({'a': Tile(0, 1), 'b': Tile(2, 3)}))
-        self.assertEqual(area, Area.from_value({'a': 0, 'b': 1, 'c': 2, 'd': 3}))
-        self.assertEqual(area, Area.from_value({'corner1': Tile(0, 1), 'corner2': Tile(2, 3)}))
+    assert area.contains(Tile(4, 2))
+    assert area.contains(Tile(3, 3))
+
+    assert not area.contains(Tile(2, 1))
+    assert not area.contains(Tile(3, 7))
+    assert not area.contains(Tile(6, 2))
+
+
+def test__iter__(area: Area):
+    tiles = (area.corner1, area.corner2)
+    for i, t in enumerate(area):
+        assert tiles[i] == t
+
+
+def test__getitem__(area: Area):
+    tiles = (area.corner1, area.corner2)
+    for i in range(2):
+        assert tiles[i] == area[i]
+
+
+def test__eq__(area: Area):
+    assert area == Area((2, 2), (5, 6))
+    assert area != Area((2, 3), (5, 6))
+
+    assert Area((3, 3)) == Area((3, 3), (3, 3))
+    assert ((3, 4), (5, 6)) == Area((3, 4), (5, 6))
+
+
+def test_from_value(area: Area):
+    area = Area((0, 1), (0, 1))
+
+    assert area == Area.from_value(Tile(0, 1))
+    assert area == Area.from_value((0, 1))
+    assert area == Area.from_value([0, 1])
+    assert area == Area.from_value({'a': 0, 'b': 1})
+    assert area == Area.from_value({'corner1': Tile(0, 1)})
+
+    area = Area((0, 1), (2, 3))
+    assert area == Area.from_value(area)
+    assert area == Area.from_value((Tile(0, 1), Tile(2, 3)))
+    assert area == Area.from_value((0, 1, 2, 3))
+    assert area == Area.from_value([0, 1, 2, 3])
+    assert area == Area.from_value([Tile(0, 1), Tile(2, 3)])
+    assert area == Area.from_value([Tile(0, 1), Tile(2, 3)])
+    assert area == Area.from_value({'a': Tile(0, 1), 'b': Tile(2, 3)})
+    assert area == Area.from_value({'a': 0, 'b': 1, 'c': 2, 'd': 3})
+    assert area == Area.from_value({'corner1': Tile(0, 1), 'corner2': Tile(2, 3)})
