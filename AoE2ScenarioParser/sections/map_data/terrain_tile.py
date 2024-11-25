54652fb5d75c8c0ea4597271fb15c82937bdb084
KSneijders <ksneijders-dev@hotmail.com>
2024-11-25 22:17:23 +0100

    Added logic for better instantiation of Structs
    Todo: move logic to generic file/class

--- AoE2ScenarioParser/sections/map_data/terrain_tile.py
+++ AoE2ScenarioParser/sections/map_data/terrain_tile.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
+from typing import Any
+
 from binary_file_parser import BaseStruct, Retriever, Version
 from binary_file_parser.types import int16, int8, uint8
 
 from AoE2ScenarioParser.objects.support import Tile
+from AoE2ScenarioParser.sections.scx_versions import MAP_LATEST
 
 
 class TerrainTile(BaseStruct):
@@ -17,10 +20,33 @@ class TerrainTile(BaseStruct):
 
     _tile: Tile
 
-    def __init__(self, struct_ver: Version = Version((2,)), initialise_defaults = True, **retriever_inits):
-        super().__init__(struct_ver, initialise_defaults = initialise_defaults, **retriever_inits)
+    # Todo: Move to general class (struct_ver as param?)
+    def merge_init_kwargs(self, locals_: dict[str, Any]):
+        kwargs = locals_.get('kwargs', {})
+        keys = (r.p_name for r in self._retrievers)
 
-        self._tile = Tile(0, 0)
+        return {
+            'struct_ver': MAP_LATEST,
+            'initialise_defaults': True,
+            **{key: locals_[key] for key in keys if key in locals_},
+            **kwargs,
+        }
+
+    # noinspection PyShadowingBuiltins
+    def __init__(
+        self,
+        type: int = 0,
+        elevation: int = 0,
+        zone: int = 0,
+        mask_type: int = -1,
+        layer_type: int = -1,
+        **kwargs
+    ):
+        values = self.merge_init_kwargs(locals())
+
+        super().__init__(**values)
+
+        self._tile = Tile(0, 0)  # To be overridden when added to a scenario using the MapManager
 
     @property
     def tile(self) -> Tile:
