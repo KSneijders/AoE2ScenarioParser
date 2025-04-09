922c1ed237449f6d80603a11d0c7a5860e5cea75
KSneijders <ksneijders-dev@hotmail.com>
2025-04-09 23:17:45 +0200

    #62: Added units prop+s and import_units function

--- AoE2ScenarioParser/managers/unit_manager.py
+++ AoE2ScenarioParser/managers/unit_manager.py
@@ -13,16 +13,127 @@ from AoE2ScenarioParser.sections import DataHeader, ScenarioSections, Settings, 
 class UnitManager(Manager):
     _struct: ScenarioSections
 
-    # Todo: Unit.player (Somehow)
-
     # @formatter:off
-    units: list[list[Unit]] = RetrieverRef(ret(ScenarioSections.unit_data), ret(UnitData.units))
+    _units: list[list[Unit]] = RetrieverRef(ret(ScenarioSections.unit_data), ret(UnitData.units))
     _next_unit_reference_id: int = RetrieverRef(ret(ScenarioSections.settings), ret(Settings.data_header), ret(DataHeader.next_unit_ref))
     # @formatter:on
+
+    @property
+    def units(self) -> list[list[Unit]]:
+        return self._units
+
+    # Todo: Add tests (player & _next_unit_reference_id)
+    @units.setter
+    def units(self, units: list[list[Unit]]):
+        if len(units) != 0 and not isinstance(units[0], list):
+            raise ValueError("list of units must be nested list")
+
+        units = [units[i] if i < len(units) else [] for i in range(9)]
+
+        highest_reference_id = self._next_unit_reference_id
+        for player in Player:
+            for unit in units[player]:
+                highest_reference_id = max(highest_reference_id, unit.reference_id + 1)
+                unit._player = player
+
+        self._units = units
+        self._next_unit_reference_id = highest_reference_id
 
     # Todo: Add tests
     def get_all_units(self) -> Generator[Unit]:
         return (unit for player_units in self.units for unit in player_units)
+
+    # Todo: Add tests
+    @property
+    def next_unit_reference_id(self) -> int:
+        """
+        Get a new unit reference ID each time the function is called. Starting from the current highest ID.
+
+        Returns:
+            The newly generated ID
+        """
+        unit_reference_id = self._next_unit_reference_id
+
+        self._next_unit_reference_id += 1
+
+        return unit_reference_id
+
+    # Todo: Add tests
+    def change_ownership(self, player: Player, units: Unit | Iterable[Unit]) -> None:
+        """
+        Change the ownership of the given units
+
+        Args:
+            player: The player to switch the units to
+            units: The units to switch ownership of
+        """
+        units = units if isinstance(units, Iterable) else [units]
+
+        self.remove_units(units)
+        self.add_units(player, units)
+
+    def add_unit(self, player: Player, unit: Unit) -> None:
+        """
+        Adds a unit for the corresponding player to the scenario
+
+        Args:
+            player: The player to add the unit for
+            unit: The unit to add
+        """
+        if not unit.has_reference_id:
+            unit.reference_id = self.next_unit_reference_id
+
+        # Set read-only player attribute for unit
+        unit._player = player
+        self.units[player].append(unit)
+
+    def add_units(self, player: Player, units: Iterable[Unit]) -> None:
+        """
+        Adds units for the corresponding player to the scenario
+
+        Args:
+            player: The player to add the units for
+            units: The units to add
+        """
+        for unit in units:
+            self.add_unit(player, unit)
+
+    # Todo: Add tests
+    def remove_unit(self, unit: Unit) -> None:
+        """
+        Removes a unit from the scenario.
+
+        Args:
+            unit: The unit to remove
+        """
+        for player_units in self.units:
+            if unit in player_units:
+                player_units.remove(unit)
+                break
+
+    # Todo: Add tests
+    def remove_units(self, units: Iterable[Unit]) -> None:
+        """
+        Removes units from the scenario.
+
+        Args:
+            units: The units to remove
+        """
+        for unit in units:
+            self.remove_unit(unit)
+
+    # Todo: Add tests
+    def import_units(self, player: Player, units: Iterable[Unit]) -> None:
+        """
+        Imports units (resetting the reference_id) for the corresponding player to the scenario.
+
+        Args:
+            player: The player to add the unit for
+            units: The units to add
+        """
+        for unit in units:
+            unit.reference_id = -1
+            self.add_unit(player, unit)
 
     # Todo: Add tests
     def filter_units_by(
@@ -31,6 +142,7 @@ class UnitManager(Manager):
         attr_values: list[int],
         is_allowlist: bool = True,
         players: Iterable[Player] = None,
+        *,
         units: Iterable[Unit] = None,
     ) -> Generator[Unit]:
         """
@@ -66,6 +178,7 @@ class UnitManager(Manager):
         unit_types: list[int],
         is_allowlist: bool = True,
         players: Iterable[Player] = None,
+        *,
         units: Iterable[Unit] = None,
     ) -> Generator[Unit]:
         """
@@ -80,7 +193,7 @@ class UnitManager(Manager):
         Returns:
             A list of units
         """
-        return self.filter_units_by("type", unit_types, is_allowlist, players, units)
+        return self.filter_units_by("type", unit_types, is_allowlist, players, units=units)
 
     # Todo: Add tests
     def filter_units_by_id(
@@ -88,6 +201,7 @@ class UnitManager(Manager):
         unit_ids: list[int],
         is_allowlist: bool = True,
         players: Iterable[Player] = None,
+        *,
         units: Iterable[Unit] = None,
     ) -> Generator[Unit]:
         """
@@ -102,15 +216,14 @@ class UnitManager(Manager):
         Returns:
             A list of units
         """
-        return self.filter_units_by("id", unit_ids, is_allowlist, players, units)
+        return self.filter_units_by("id", unit_ids, is_allowlist, players, units=units)
 
-    # Todo: Add tests
     def get_units_in_area(
         self,
         area: Area,
         players: Iterable[Player] = None,
         *,
-        units: list[Unit] = None,
+        units: Iterable[Unit] = None,
     ) -> Generator[Unit]:
         """
         Get all units in a given area. Optionally filter by given players.
@@ -131,90 +244,6 @@ class UnitManager(Manager):
             return (unit for unit in units if area.contains(unit.tile))
 
         return (unit for unit in units if area.contains(unit.tile) and unit.player in players)
-
-    # Todo: Add tests
-    @property
-    def next_unit_reference_id(self):
-        """
-        Get a new unit reference ID each time the function is called. Starting from the current highest ID.
-
-        Returns:
-            The newly generated ID
-        """
-        unit_reference_id = self._next_unit_reference_id
-
-        self._next_unit_reference_id += 1
-
-        return unit_reference_id
-
-    # Todo: Add tests
-    def change_ownership(self, player: Player, units: Unit | Iterable[Unit]) -> None:
-        """
-        Change the ownership of the given units
-
-        Args:
-            player: The player to switch the units to
-            units: The units to switch ownership of
-        """
-        units = units if isinstance(units, Iterable) else [units]
-
-        self.remove_units(units)
-        self.add_units(player, units)
-
-    # Todo: Add import_units functionality which resets all reference IDs
-
-    # Todo: Add tests
-    def add_unit(self, player: Player, unit: Unit) -> None:
-        """
-        Adds a unit for the corresponding player to the scenario
-
-        Args:
-            player: The player to add the unit for
-            unit: The unit to add
-        """
-        if not unit.has_reference_id:
-            unit.reference_id = self.next_unit_reference_id
-
-        # Todo: Add player attr tests
-        # Set read-only player attribute for unit
-        unit._player = player
-        self.units[player].append(unit)
-
-    # Todo: Add tests
-    def add_units(self, player: Player, units: Iterable[Unit]) -> None:
-        """
-        Adds units for the corresponding player to the scenario
-
-        Args:
-            player: The player to add the units for
-            units: The units to add
-        """
-        for unit in units:
-            self.add_unit(player, unit)
-
-    # Todo: Add tests
-    def remove_unit(self, unit: Unit) -> None:
-        """
-        Removes a unit from the scenario.
-
-        Args:
-            unit: The unit to remove
-        """
-        for player_units in self.units:
-            if unit in player_units:
-                player_units.remove(unit)
-                break
-
-    # Todo: Add tests
-    def remove_units(self, units: Iterable[Unit]) -> None:
-        """
-        Removes units from the scenario.
-
-        Args:
-            units: The units to remove
-        """
-        for unit in units:
-            self.remove_unit(unit)
 
     def apply_global_offset(
         self,
