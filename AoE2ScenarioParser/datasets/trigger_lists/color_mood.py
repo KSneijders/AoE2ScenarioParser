57b42a404719f3355d49b3c108b306823a5ff296
KSneijders <ksneijders-dev@hotmail.com>
2025-06-12 20:02:11 +0200

    Adds new additional color moods
    Credits: goku2134 @ Discord

--- AoE2ScenarioParser/datasets/trigger_lists/color_mood.py
+++ AoE2ScenarioParser/datasets/trigger_lists/color_mood.py
@@ -19,3 +19,16 @@ class ColorMood(_DataSetIntEnums):
     JUNGLE = 3
     DESERT = 4
     NIGHT = 5
+    EVENING = 6
+    SPRING = 7
+    SAVANNAH = 8
+    ARCTIC = 9
+    RAINFOREST = 10
+    SWAMP = 11
+    STEPPES = 12
+    MISTY = 13
+    SUMMER = 14
+    MURKY = 15
+    BRUMOUS = 16
+    TWILIGHT = 17
+    DARKNESS = 18
