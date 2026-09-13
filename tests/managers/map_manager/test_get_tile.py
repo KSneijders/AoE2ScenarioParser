dd52eedb6706d2c11ccb960142b820b43f8dd92a
KSneijders <ksneijders-dev@hotmail.com>
2026-09-13 13:43:57 +0200

    Removes top level objects folder in tests

--- tests/managers/map_manager/test_get_tile.py
+++ tests/managers/map_manager/test_get_tile.py
@@ -6,5 +6,4 @@ def test_get_tile(mm: MapManager):
     assert mm.get_tile(tile = (0, 0)) is mm.terrain[0][0]
 
     assert mm.get_tile(i = 20) is mm.terrain[4][0]
-
-    assert mm.get_tile(i = 20) is mm.terrain[4][0]
+    assert mm.get_tile(tile = (0, 4)) is mm.terrain[4][0]
